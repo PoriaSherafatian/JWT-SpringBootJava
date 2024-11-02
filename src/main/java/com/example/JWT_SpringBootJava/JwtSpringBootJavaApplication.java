0a45d69618fa9afd7ba265a46f2c29ad96adaebc
PoriaSherafatian <poriasherafatian@gmail.com>
2024-11-02 20:59:08 +0330

    first commit

---
+++ src/main/java/com/example/JWT_SpringBootJava/JwtSpringBootJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.JWT_SpringBootJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSpringBootJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtSpringBootJavaApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/JWT_SpringBootJava/user/Role.java
@@ -0,0 +1,7 @@
+package com.example.JWT_SpringBootJava.user;
+
+public enum Role {
+
+    USER, ADMIN
+
+}
